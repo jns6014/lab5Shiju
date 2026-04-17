2cc50577300d79ff19e61978fe698652a20adf57
josephshiju <jns6014@psu.edu>
2026-04-16 20:29:45 -0400

    lab5

--- src/Receiver.java
+++ src/Receiver.java
@@ -1,4 +1,36 @@
-package PACKAGE_NAME;
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
+import java.util.Scanner;
 
 public class Receiver {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+        // Simulate receiving message + HMAC
+        System.out.print("Enter received message: ");
+        String message = sc.nextLine();
+        System.out.print("Enter received HMAC: ");
+        String receivedHmac = sc.nextLine();
+        String key = "Joe";
+
+        // Generate HMAC again
+        Mac mac = Mac.getInstance("HmacSHA256");
+        mac.init(new SecretKeySpec(key.getBytes(), "HmacSHA256"));
+        String generatedHmac = Base64.getEncoder()
+                .encodeToString(mac.doFinal(message.getBytes()));
+
+        // Output everything
+        System.out.println("\n=== RECEIVER ===");
+        System.out.println("Received Message: " + message);
+        System.out.println("Received HMAC: " + receivedHmac);
+        System.out.println("Generated HMAC: " + generatedHmac);
+
+        // CORRECT comparison
+        if (generatedHmac.equals(receivedHmac)) {
+            System.out.println("VALID MESSAGE");
+        } else {
+            System.out.println("TAMPERED MESSAGE");
+        }
+    }
 }
--- src/Sender.java
+++ src/Sender.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
 
 public class Sender {
+    public static void main(String[] args) throws Exception {
+        String key = "Joe";
+        String message = "Hello_Earth";
+
+        // Create HMAC using SHA-256
+        Mac mac = Mac.getInstance("HmacSHA256");
+        mac.init(new SecretKeySpec(key.getBytes(), "HmacSHA256"));
+        String hmac = Base64.getEncoder()
+                .encodeToString(mac.doFinal(message.getBytes()));
+
+        // Output what is being "sent"
+        System.out.println("=== SENDER ===");
+        System.out.println("Message Sent: " + message);
+        System.out.println("HMAC Sent: " + hmac);
+    }
 }
